6907acc7c1b0ad39693be12dab651bdf059eb492
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-02-27 14:08:50 -0300

    Second Commit

--- src/main/java/io/github/prenitavulpo/HelloApplication.java
+++ src/main/java/io/github/prenitavulpo/HelloApplication.java
@@ -3,10 +3,18 @@ package io.github.prenitavulpo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "hello world";
     }
 }
