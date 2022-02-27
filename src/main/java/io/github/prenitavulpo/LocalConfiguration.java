590cda00e2dfad19f1618fa1783eb985ee240e8f
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-02-27 15:41:06 -0300

    Final commit

--- src/main/java/io/github/prenitavulpo/LocalConfiguration.java
+++ src/main/java/io/github/prenitavulpo/LocalConfiguration.java
@@ -1,13 +1,14 @@
 package io.github.prenitavulpo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LocalConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Hello Spring";
+    @Bean
+    public CommandLineRunner exec(){
+        return args -> System.out.println("Development Build");
     }
 }
