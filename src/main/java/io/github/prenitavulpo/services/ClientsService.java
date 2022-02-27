39d9c3a5c58e25b3dd7bdc5d7b9d210449e3bb69
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-02-27 15:05:49 -0300

    Add repository on the Service constructor

--- src/main/java/io/github/prenitavulpo/services/ClientsService.java
+++ src/main/java/io/github/prenitavulpo/services/ClientsService.java
@@ -2,14 +2,22 @@ package io.github.prenitavulpo.services;
 
 import io.github.prenitavulpo.models.Client;
 import io.github.prenitavulpo.repositories.ClientsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientsService {
+
+    private ClientsRepository clientsRepository;
+
+    @Autowired
+    public ClientsService(ClientsRepository clientsRepository){
+        this.clientsRepository = clientsRepository;
+    }
+
     public void saveClient(Client client){
         validadeClient(client);
-        ClientsRepository clientsRepository = new ClientsRepository();
-        clientsRepository.persistClient(client);
+        this.clientsRepository.persistClient(client);
     }
 
     public void validadeClient(Client client){}
